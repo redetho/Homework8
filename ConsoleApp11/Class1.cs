9f9b13ec1ffca0082db2a24d56241915fc68a173
Thomas Redekop <redetho@gmail.com>
2021-10-06 22:34:00 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp11/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp11
+{
+    class pessoa
+    {
+        //declara nome e email dentro de Pessoa
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        //constructor
+        public pessoa(string nome, string email) 
+        {
+            Nome = nome;
+            Email = email;
+        }
+        //override
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            pessoa[] vect = new pessoa[10];//declara numero de quartos
+
+            Console.WriteLine("quantos quartos serão alugados?: ");
+            int n = int.Parse(Console.ReadLine());//coleta e separa a quantidade de quartos salvos.
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();//pula linha
+                Console.WriteLine($"hospede #{ i}");
+                Console.WriteLine("Nome");//recebe nome
+                string nome = Console.ReadLine();
+                Console.WriteLine("Email");//recebe email
+                string email = Console.ReadLine();
+                Console.WriteLine("Quarto");//recebe quarto
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new pessoa(nome, email);//salva no vector
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i<10; i++)
+            {
+                if(vect[i] != null)//se possui algo dentro de tal vector, imprime itens do vector
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+        }
+    }
+}
